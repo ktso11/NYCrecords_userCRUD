48419d9ea0e4743626f2b9e936bbe5ed7faddb05
ktso11 <sokatie11@gmail.com>
2018-05-22 09:37:29 -0700

    removed http comments from user service, using httpclient completely

--- nyc-frontend/src/app/user.service.ts
+++ nyc-frontend/src/app/user.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {User} from './types/user';
-// import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,33 +9,22 @@ export class UserService {
 
   constructor(
     private httpClient: HttpClient,
-    // private http: Http
   ) { }
 
   getAllUsers() {
     return this.httpClient.get<User[]>('/api/users');
-    // return this.http.get('/api/users');
   }
 
   get(id: string) {
-    // return this.http.get('/api/users/' + id);
     return this.httpClient.get<User>(`/api/users/${id}`);
   }
 
   add(user: User){
     return this.httpClient.post<User>(
       '/api/users', user
-    // return this.http.post('/api/users',{
-      // "username": user.username,
-      // "firstname": user.firstname,
-      // "lastname": user.lastname,
-      // "email": user.email
-    // }
     );
   }
-
   remove(id: string){
-    // return this.http.delete('/api/users/'+ id);
     return this.httpClient.delete(`/api/users/${id}`);
   }
 
@@ -44,10 +32,5 @@ export class UserService {
     return this.httpClient.patch(`/api/users/` + updateduser.id,
     updateduser
     );
-    // return this.http.patch('/api/users/' + user.id,{
-    //   "firstname": user.firstname,
-    //   "lastname": user.lastname,
-    //   "email": user.email
-    // });
-    }
+  }
 }
